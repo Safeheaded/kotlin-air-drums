3515edfa9cb35d2d90d4593496c27d32f58974c0
Patryk Marczak <m.patryk@outlook.com>
2023-05-06 22:26:17 +0200

    Added all dropdowns

--- app/src/main/java/com/example/airdrums/SettingsActivity.kt
+++ app/src/main/java/com/example/airdrums/SettingsActivity.kt
@@ -1,33 +1,70 @@
 package com.example.airdrums
 
 import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
-    val items: Array<String> = arrayOf("test1", "test2", "test3", "test4")
+    val northItems: Array<String> = arrayOf("north1", "north2", "north3", "north4")
+    val westItems: Array<String> = arrayOf("west1", "west2", "west3", "west4")
+    val southItems: Array<String> = arrayOf("south1", "south2", "south3", "south4")
+    val eastItems: Array<String> = arrayOf("east1", "east2", "east3", "east4")
 
-    private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var autoCompleteTextViewNorth: AutoCompleteTextView
+    private lateinit var autoCompleteTextViewWest: AutoCompleteTextView
+    private lateinit var autoCompleteTextViewSouth: AutoCompleteTextView
+    private lateinit var autoCompleteTextViewEast: AutoCompleteTextView
 
-    private lateinit var adapterItems: ArrayAdapter<String>;
+    private lateinit var adapterItemsNorth: ArrayAdapter<String>;
+    private lateinit var adapterItemsWest: ArrayAdapter<String>;
+    private lateinit var adapterItemsSouth: ArrayAdapter<String>;
+    private lateinit var adapterItemsEast: ArrayAdapter<String>;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
-        autoCompleteTextView = findViewById(R.id.auto_complete_text)
-        adapterItems = ArrayAdapter<String>(this, R.layout.select_item, items)
-        autoCompleteTextView.setAdapter(adapterItems)
+        autoCompleteTextViewNorth = findViewById(R.id.auto_complete_text)
+        autoCompleteTextViewWest = findViewById(R.id.auto_complete_text1)
+        autoCompleteTextViewSouth = findViewById(R.id.auto_complete_text2)
+        autoCompleteTextViewEast = findViewById(R.id.auto_complete_text3)
+        adapterItemsNorth = ArrayAdapter<String>(this, R.layout.select_item, northItems)
+        adapterItemsWest = ArrayAdapter<String>(this, R.layout.select_item, westItems)
+        adapterItemsSouth = ArrayAdapter<String>(this, R.layout.select_item, southItems)
+        adapterItemsEast = ArrayAdapter<String>(this, R.layout.select_item, eastItems)
+        autoCompleteTextViewNorth.setAdapter(adapterItemsNorth)
+        autoCompleteTextViewWest.setAdapter(adapterItemsWest)
+        autoCompleteTextViewSouth.setAdapter(adapterItemsSouth)
+        autoCompleteTextViewEast.setAdapter(adapterItemsEast)
 
-        autoCompleteTextView.setText("test1", false)
+        autoCompleteTextViewNorth.setText(adapterItemsNorth.getItem(0), false)
+        autoCompleteTextViewWest.setText(adapterItemsWest.getItem(0), false)
+        autoCompleteTextViewSouth.setText(adapterItemsSouth.getItem(0), false)
+        autoCompleteTextViewEast.setText(adapterItemsEast.getItem(0), false)
 
-        autoCompleteTextView.onItemClickListener =
+        autoCompleteTextViewNorth.onItemClickListener =
+            AdapterView.OnItemClickListener() { parent, view, position, id ->
+                val item = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@SettingsActivity, "Item: $item", Toast.LENGTH_SHORT).show()
+            }
+
+        autoCompleteTextViewWest.onItemClickListener =
+            AdapterView.OnItemClickListener() { parent, view, position, id ->
+                val item = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@SettingsActivity, "Item: $item", Toast.LENGTH_SHORT).show()
+            }
+
+        autoCompleteTextViewSouth.onItemClickListener =
+            AdapterView.OnItemClickListener() { parent, view, position, id ->
+                val item = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@SettingsActivity, "Item: $item", Toast.LENGTH_SHORT).show()
+            }
+
+        autoCompleteTextViewEast.onItemClickListener =
             AdapterView.OnItemClickListener() { parent, view, position, id ->
                 val item = parent.getItemAtPosition(position).toString()
                 Toast.makeText(this@SettingsActivity, "Item: $item", Toast.LENGTH_SHORT).show()
